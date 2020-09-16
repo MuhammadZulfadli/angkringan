1da7cb46630da0d02c8bf73ee3b30269d30632c2
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-09-16 10:53:39 +0700

    add plugin

---
+++ angkringan/src/components/Cart.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="max-auto" max-width="300" tile>
+    <v-list rounded>
+      <v-subheader>Cart</v-subheader>
+      <v-list-item-group v-model="item" color="primary">
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- angkringan/src/views/Footer.vue
+++ angkringan/src/views/Footer.vue
@@ -3,8 +3,9 @@
     <v-card height="150">
       <v-footer absolute class="font-weight-medium">
         <v-col class="text-center" cols="12">
-          <v-icon>mdi-copyright</v-icon> {{ new Date().getFullYear() }} —
-          <strong>Angkringan 4.1</strong>
+          <v-icon>mdi-copyright</v-icon>
+          {{ new Date().getFullYear() }} —
+          <strong>Angkringan 4.0</strong>
         </v-col>
       </v-footer>
     </v-card>
