9c71edde7b1252f2ca58e486f69fad044ed87419
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-09-18 13:01:59 +0700

    finished all of them

--- angkringan/src/components/FinishedTransaction.vue
+++ angkringan/src/components/FinishedTransaction.vue
@@ -2,8 +2,9 @@
   <div>
     <v-card class="mx-auto" max-width="1000" cols="12" md="12" sm="12">
       <v-card-text>
-        <p class="display-1 text--primary">Silahkan ke kasir, dan selesaikan transaksi kamu!</p>
-        <div class="text--primary">Jika sudah, silahkan tunggu pesanan mu</div>
+        <p class="display-1 text--primary">Silahkan ke kasir, selesaikan transaksi kamu!!</p>
+        <!-- <div class="text--primary">Dan silahkan tunggu pesanan mu</div> -->
+        <h4 class="text--primary">Dan silahkan tunggu, pesanan mu segera datang..</h4>
       </v-card-text>
       <v-row align="center" justify="center" cols="12" md="12" sm="12">
         <v-card-actions>
@@ -11,6 +12,7 @@
           <v-img rounded src="../assets/success.png" alt="Success" max-width="500" max-height="300"></v-img>
         </v-card-actions>
       </v-row>
+      <p class="display-1 text--primary">Terima kasih sudah menjadi pelanggan setia kami.</p>
     </v-card>
   </div>
 </template>
