377e4044f7e139de581a5d2f36383f372b030068
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-09-21 17:11:56 +0700

    update drawer

--- angkringan/src/components/Appbar.vue
+++ angkringan/src/components/Appbar.vue
@@ -15,9 +15,9 @@
         <v-badge color="primary" :content="jumlah"></v-badge>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" absolute temporary>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
       <v-list nav dense>
-        <v-list-item-group active-class="deep-purple--text text--accent-4">
+        <v-list-item-group active-class=" text--accent-4">
           <v-list-item v-for="link in links" :key="link.text" :to="link.route">
             <v-list-item-icon>
               <v-icon>{{ link.icon }}</v-icon>
