c6132022d5171e84fc956dbd200c7ee35495932f
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-09-24 17:13:07 +0700

    completed navigation

--- angkringan/src/components/FinishedTransaction.vue
+++ angkringan/src/components/FinishedTransaction.vue
@@ -1,41 +1,43 @@
 <template>
   <div>
-    <v-card-title class="headline">Transaksi Kamu</v-card-title>
-    <v-simple-table fixed-header height="300px">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th>ID Transaksi</th>
-            <th>Tanggal Transaksi</th>
-            <th>Nama Item</th>
-            <th>Keterangan</th>
-            <th>Harga</th>
-            <th>Jumlah Pesanan</th>
-            <th>Harga Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, index) in card" :key="item.id">
-            <td>{{ index + 1 }}</td>
-            <td>{{ tgl }}</td>
-            <td>{{ item.item }}</td>
-            <td>{{ item.keterangan }}</td>
-            <td>{{ item.harga }}</td>
-            <td>{{ item.qty }}</td>
-            <td>{{ item.qty * item.harga }}</td>
-          </tr>
-          <tr>
-            <td colspan="4">Total Pesanan</td>
-            <td></td>
-            <td>{{ jml }}</td>
-          </tr>
-          <tr>
-            <td colspan="6">Harga Total :</td>
-            <td>{{ jumlahTot }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <v-container>
+      <v-card-title class="headline">Transaksi Kamu</v-card-title>
+      <v-simple-table fixed-header height="300px">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th>ID Transaksi</th>
+              <th>Tanggal Transaksi</th>
+              <th>Nama Item</th>
+              <th>Keterangan</th>
+              <th>Harga</th>
+              <th>Jumlah Pesanan</th>
+              <th>Harga Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in card" :key="item.id">
+              <td>{{ index + 1 }}</td>
+              <td>{{ tgl }}</td>
+              <td>{{ item.item }}</td>
+              <td>{{ item.keterangan }}</td>
+              <td>{{ item.harga }}</td>
+              <td>{{ item.qty }}</td>
+              <td>{{ item.qty * item.harga }}</td>
+            </tr>
+            <tr>
+              <td colspan="4">Total Pesanan</td>
+              <td></td>
+              <td>{{ jml }}</td>
+            </tr>
+            <tr>
+              <td colspan="6">Harga Total :</td>
+              <td>{{ jumlahTot }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-container>
   </div>
 </template>
 
